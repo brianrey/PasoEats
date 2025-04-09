598565b5ca10ba0dbfee00914c8606fdb5e3873e
Nobody <165957643+Nobody277@users.noreply.github.com>
2025-04-09 15:00:26 -0700

    Create Course.java

---
+++ Course.java
@@ -0,0 +1,39 @@
+public class Course {
+    private String courseName;
+    private String roomNum;
+    private String meetTime;
+    private String instructor;
+
+
+    public void setCourseName(String courseName){
+        this.courseName = courseName;
+    }
+
+    public void setRoomNum(String roomNum){
+        this.roomNum = roomNum;
+    }
+
+    public void setMeetTime(String meetTime){
+        this.meetTime = meetTime;
+    }
+
+    public void setInstructor(String instructor){
+        this.instructor = instructor;
+    }
+
+    public String getCourseName(String courseName){
+        return courseName;
+    }
+
+    public String getRoomNum(String roomNum){
+        return roomNum;
+    }
+
+    public String getMeetTime(String meetTime){
+        return meetTime;
+    }
+
+    public String getInstructor(String instructor){
+        return instructor;
+    }
+}
