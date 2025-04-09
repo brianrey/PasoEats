d49feda6170613ee048ec10599fcd77486548cfd
Nobody <165957643+Nobody277@users.noreply.github.com>
2025-04-09 15:22:41 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-    Course course = new Course("CS1", "810", "2:00pm", "Professor Dube");
+    Course course = new Course("CS1", "810", "2:00pm", "Professor Dube", "Monday - Wednsday");
 
     }
 }
